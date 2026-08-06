1d74bd480e7d208ed2d414bf81a1277b996e136b
yahiralmendras22 <yahiralmendras22@gmail.com>
2026-08-06 13:00:12 -0300

    Actualizar variables de entorno en archivos de configuración para incluir sufijos de milisegundos

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -6,13 +6,13 @@ if (process.env.NODE_ENV !== 'production') {
 }
 const configPool = {
     host: process.env.DB_HOST,
-    database: process.env.DB_NAME, 
-    port: parseInt(process.env.DB_PORT, 10), 
+    port: parseInt(process.env.DB_PORT, 10),
+    database: process.env.DB_NAME,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    max: parseInt(process.env.DB_MAX_CONNECTIONS, 10), 
-    idleTimeoutMillis: parseInt(process.env.DB_IDLE_TIMEOUT, 10), 
-    connectionTimeoutMillis: parseInt(process.env.DB_CONNECTION_TIMEOUT, 10), 
+    max: parseInt(process.env.DB_MAX_CONNECTIONS, 10),
+    idleTimeoutMillis: parseInt(process.env.DB_IDLE_TIMEOUT_MILLIS, 10),
+    connectionTimeoutMillis: parseInt(process.env.DB_CONNECTION_TIMEOUT_MILLIS, 10)
 };
 
 const pool = new Pool(configPool); 
