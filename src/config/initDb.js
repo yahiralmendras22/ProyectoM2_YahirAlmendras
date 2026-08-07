df3a849ad9a88977dedd351f8f1f516c1d57fcec
yahiralmendras22 <yahiralmendras22@gmail.com>
2026-08-07 16:13:46 -0300

    fix: mejorar la inicialización de la base de datos y ajustar mensajes de consola

--- src/config/initDb.js
+++ src/config/initDb.js
@@ -1,23 +1,25 @@
+const { loadEnvFile } = require('node:process');
 const fs = require('node:fs');
 const path = require('node:path');
+
+if (fs.existsSync('.env')) {
+    loadEnvFile('.env');
+}
+
 const { pool } = require('./dbConnect');
 
 async function initializeDatabase() {
     try {
         const sqlPath = path.join(__dirname, '..', 'sql', 'setup.sql');
-
         const sqlQueries = fs.readFileSync(sqlPath, 'utf-8');
 
         console.log('Iniciando la creación de tablas...');
-
         await pool.query(sqlQueries);
-
-        console.log('¡Tablas creadas con éxito en blog_db!');
+        console.log('¡Tablas creadas con éxito!');
     } catch (error) {
         console.error('Error al inicializar la base de datos:', error.message);
-    } finally {
-        await pool.end();
     }
+    // No cerramos el pool aquí para que Express pueda seguir usándolo inmediatamente
 }
 
 initializeDatabase();
