04e0e0abbf4bbd09c4221b2da13118cfe09e626b
yahiralmendras22 <yahiralmendras22@gmail.com>
2026-08-07 12:05:45 -0300

    feat: add Swagger documentation for MiniBlog API and update dependencies
    - Added Swagger UI integration to serve API documentation.
    - Created OpenAPI specification in docs/openapi.yaml for authors and posts endpoints.
    - Updated package.json to include new dependencies: pg, swagger-ui-express, and yamljs.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 const express = require('express');
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+
 const app = express();
+
+// Swagger
+const swaggerDocument = YAML.load('./docs/openapi.yaml');
+
+app.use(
+    '/api-docs',
+    swaggerUi.serve,
+    swaggerUi.setup(swaggerDocument)
+);
 
 // Middleware para entender JSON
 app.use(express.json());
 
-// RUTAS CORREGIDAS (Sin el "./src/")
+// Rutas
 const authorsRouter = require('./routes/authorsRoutes');
 const postsRouter = require('./routes/postsRoutes');
 
@@ -12,10 +24,8 @@ const postsRouter = require('./routes/postsRoutes');
 app.use('/authors', authorsRouter);
 app.use('/posts', postsRouter);
 
-
+// Middleware de manejo de errores
 const errorHandler = require('./middlewares/errorHandler');
-
 app.use(errorHandler);
-
 
 module.exports = app;
