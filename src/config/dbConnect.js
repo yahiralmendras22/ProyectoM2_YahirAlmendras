6a84e690d657032cc6e27a268a832b6bafd25855
yahiralmendras22 <yahiralmendras22@gmail.com>
2026-08-07 13:06:54 -0300

    feat: actualizar configuración de conexión a la base de datos para usar DATABASE_URL

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -4,20 +4,23 @@ const { loadEnvFile } = require('node:process');
 if (process.env.NODE_ENV !== 'production') {
     loadEnvFile('.env');
 }
-const configPool = {
-    host: process.env.DB_HOST,
-    port: parseInt(process.env.DB_PORT, 10),
-    database: process.env.DB_NAME,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    max: parseInt(process.env.DB_MAX_CONNECTIONS, 10),
-    idleTimeoutMillis: parseInt(process.env.DB_IDLE_TIMEOUT_MILLIS, 10),
-    connectionTimeoutMillis: parseInt(process.env.DB_CONNECTION_TIMEOUT_MILLIS, 10)
-};
+
+
+const configPool = process.env.DATABASE_URL 
+    ? { connectionString: process.env.DATABASE_URL }
+    : {
+        host: process.env.DB_HOST,
+        port: parseInt(process.env.DB_PORT, 10),
+        database: process.env.DB_NAME,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        max: parseInt(process.env.DB_MAX_CONNECTIONS, 10),
+        idleTimeoutMillis: parseInt(process.env.DB_IDLE_TIMEOUT_MILLIS, 10),
+        connectionTimeoutMillis: parseInt(process.env.DB_CONNECTION_TIMEOUT_MILLIS, 10)
+      };
 
 const pool = new Pool(configPool); 
 
 module.exports = {
     pool
 };
-    
