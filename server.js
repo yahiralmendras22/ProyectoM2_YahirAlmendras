b506e63ae842235e25d0a02653da4bbdb3eb2bd9
yahiralmendras22 <yahiralmendras22@gmail.com>
2026-08-12 15:58:05 -0300

    fix: mejorar comentarios en server.js para mayor claridad sobre la carga de variables de entorno

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const { loadEnvFile } = require('node:process');
 const fs = require('node:fs');
 
-// Verifica si el archivo existe antes de intentar cargarlo
+// Esto lee tu archivo local .env y carga las variables
 if (fs.existsSync('.env')) {
-    loadEnvFile('.env');
+    loadEnvFile('.env'); 
 }
 
 const app = require('./src/index.js');
-
-const PORT = process.env.PORT || 3000;
+// Si process.env.PORT existe (gracias al .env local o a Railway), usará ese. Si no, usará 3000.
+const PORT = process.env.PORT || 3000; 
 
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server listening on port ${PORT}`);
